543999ad5741b41f01babe3ba1aa465aab24aab9
soraia-aparecida <soraia211norte@gmail.com>
2022-03-23 17:03:39 -0300

    separação do app, para uma pasta a parte

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,9 @@
-import { AddressInfo } from "net"
-import express, { Express } from "express"
-import cors from "cors"
-import dotenv from "dotenv"
 import { userRouter } from "./router/userRouter"
 import { moduleRouter } from "./router/moduleRouter"
 import { classeRouter } from "./router/classeRouter"
-
-dotenv.config()
-
-const app: Express = express();
-app.use(express.json())
-app.use(cors())
+import { app } from "./app"
 
 app.use('/user', userRouter)
 app.use('/module', moduleRouter)
 app.use('/classe', classeRouter)
-
-const server = app.listen(process.env.PORT || 3003, () => {
-    if (server) {
-        const address = server.address() as AddressInfo;
-        console.log(`Server is running in http://localhost: ${address.port}`)
-    } else {
-        console.error(`Failure upon starting server.`)
-    }
-})
 
