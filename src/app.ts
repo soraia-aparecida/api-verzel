01018e0ae8c335e46dab56683339fb046803a26e
soraia-aparecida <soraia211norte@gmail.com>
2022-03-23 17:18:15 -0300

    corre~ção do tsconfig

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,21 @@
-import express from "express"
+import { AddressInfo } from "net"
+import express, { Express } from "express"
 import cors from "cors"
 import dotenv from "dotenv"
 
 dotenv.config()
-export const app = express()
 
+const app: Express = express();
 app.use(express.json())
 app.use(cors())
 
-app.listen(process.env.PORT || 3003, () => {
-  console.log("Servidor rodando na porta 3003")
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+        const address = server.address() as AddressInfo;
+        console.log(`Server is running in http://localhost: ${address.port}`)
+    } else {
+        console.error(`Failure upon starting server.`)
+    }
 })
+
+export default app
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { userRouter } from "./router/userRouter"
 import { moduleRouter } from "./router/moduleRouter"
 import { classeRouter } from "./router/classeRouter"
-import { app } from "./app"
+import app  from "./app"
 
 app.use('/user', userRouter)
 app.use('/module', moduleRouter)
