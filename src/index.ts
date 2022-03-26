dc09b566decd41b545ab8f30c3d2ab717f9c956b
soraia-aparecida <soraia211norte@gmail.com>
2022-03-26 14:49:27 -0300

    problema no cors, informei a url que pode acessar

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,8 @@ app.use(express.json())
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "*")
+    res.header("Access-Control-Max-Age", "86400")
     app.use(cors());
     next();
 });
