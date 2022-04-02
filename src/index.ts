252ef6d18156020ed20ff01efde84d5568ccd525
Soraia Aparecida Chaves Cordeiro Lima <91444881+soraia-aparecida@users.noreply.github.com>
2022-04-01 21:02:02 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const app: Express = express();
 app.use(express.json())
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     res.header("Access-Control-Allow-Headers", "*")
     app.use(cors());
